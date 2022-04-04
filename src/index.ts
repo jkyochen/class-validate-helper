922872ce4fd045c47eee13c2f5b492d484e6f5eb
Lanlyhs <ziqiang.chen@outlook.com>
2022-04-04 14:51:02 +0800

    Init class-validate-helper

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
+import "reflect-metadata";
+
+export function Validate() {
+    const validateParamIndex = 0;
+    return function (target: any, propertyName: string, descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<void>>) {
+        let originalMethod = descriptor.value;
+        descriptor.value = async function (...args: any[]) {
+            const ParamTypes = Reflect.getMetadata('design:paramtypes', target, propertyName);
+            const ParamType = ParamTypes[validateParamIndex];
+            let param = plainToInstance(ParamType, args[validateParamIndex]);
+            let errors = await validate(param);
+            if (errors.length) {
+                const error: any = new Error(errors.toString());
+                error.errors = errors;
+                error.paramName = propertyName;
+                throw error;
+            }
+            if (!originalMethod) {
+                return;
+            }
+            originalMethod.apply(this, args);
+        };
+    };
+}
