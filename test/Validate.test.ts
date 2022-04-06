e40f3c144b7f43b6d2eddc809cfe3a2c1f816b0c
Lanlyhs <ziqiang.chen@outlook.com>
2022-04-06 22:25:55 +0800

    Update add fail test

--- test/Validate.test.ts
+++ test/Validate.test.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 import { Validate } from '../src';
 
 describe("Validate", () => {
@@ -16,6 +16,22 @@ describe("Validate", () => {
         }
         let controller = new Controller();
         expect(await controller.getUser({name: ""})).toEqual("");
+    });
+
+    it("Validate fail expect throw error", async () => {
+        class User {
+            @IsString()
+            @IsEmail()
+            name: string
+        }
+        class Controller {
+            @Validate()
+            async getUser(user: User) {
+                return user.name;
+            }
+        }
+        let controller = new Controller();
+        await expect(controller.getUser({name: ""})).rejects.toThrow();
     });
 
 });
